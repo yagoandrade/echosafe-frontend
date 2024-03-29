cc83a51a7f19304507bd5a33bce3cffdaafe0e35
Yago Andrade <contact@yagoandrade.com>
2024-03-29 02:14:39 -0300

    feat(auth): ui for login and register pages

--- src/app/api/auth/register/page.tsx
+++ src/app/api/auth/register/page.tsx
@@ -5,6 +5,19 @@ import { api } from "@/trpc/react";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import SignInWithGoogleButton from "@/components/sign-in-with-google";
+
 export default function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -29,34 +42,68 @@ export default function Register() {
 
   return (
     <form
+      className="flex h-full min-h-screen w-full items-center justify-center"
       onSubmit={(e) => {
         e.preventDefault();
         registerUser.mutate({ name, email, password });
       }}
     >
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="name"
-        required
-      />
-      {errors.name && <span>This field is required</span>}
-      <input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="email"
-        required
-      />
-      {errors.email && <span>This field is required</span>}
-      <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        type="password"
-        required
-      />
-      {errors.password && <span>This field is required</span>}
-      <button type="submit">Register</button>
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your information to create an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="name">Full name</Label>
+              <Input
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="John Doe"
+                required
+              />
+              {errors.name && <span>This field is required</span>}
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="me@example.com"
+                required
+              />
+              {errors.email && <span>This field is required</span>}
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              {errors.password && <span>This field is required</span>}
+            </div>
+            <Button type="submit" className="w-full">
+              Create an account
+            </Button>
+            <SignInWithGoogleButton />
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Link href="/api/auth/signin?csrf=true" className="underline">
+              Sign in
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
     </form>
   );
 }
--- src/app/api/auth/signin/page.tsx
+++ src/app/api/auth/signin/page.tsx
@@ -3,6 +3,19 @@
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import SignInWithGoogleButton from "@/components/sign-in-with-google";
+import Link from "next/link";
 
 export default function SignInPage() {
   const { data: session, status } = useSession();
@@ -25,19 +38,53 @@ export default function SignInPage() {
   }
 
   return (
-    <>
-      <button onClick={() => signIn("google")}>Sign in with Google</button>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("email", { required: true })} placeholder="Email" />
-        {errors.email && <span>This field is required</span>}
-        <input
-          {...register("password", { required: true })}
-          placeholder="Password"
-          type="password"
-        />
-        {errors.password && <span>This field is required</span>}
-        <button type="submit">Sign in with Email</button>
-      </form>
-    </>
+    <form
+      className="flex h-full min-h-screen w-full items-center justify-center"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="me@example.com"
+                required
+                {...register("email", { required: true })}
+              />
+              {errors.email && <span>This field is required</span>}
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                required
+                {...register("password", { required: true })}
+              />
+              {errors.password && <span>This field is required</span>}
+            </div>
+            <Button type="submit" className="w-full">
+              Sign in
+            </Button>
+            <SignInWithGoogleButton />
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account yet?{" "}
+            <Link href="/api/auth/register" className="underline">
+              Register here
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </form>
   );
 }
