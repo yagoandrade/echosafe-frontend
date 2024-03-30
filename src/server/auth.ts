f3a9a657c1da4beaae6b6b4eeaa928a0fd25da2b
Yago Andrade <contact@yagoandrade.com>
2024-03-30 03:58:46 -0300

    feat: remove debug mode from production

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -56,7 +56,7 @@ export const authOptions: NextAuthOptions = {
   },
   session: { strategy: "jwt" },
   secret: process.env.NEXTAUTH_SECRET,
-  debug: /* process.env.NODE_ENV === "development" */ true,
+  debug: process.env.NODE_ENV === "development",
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     CredentialsProvider({
