125ee91d1650eca4a1dae9270f8feadaa748bd89
Yago Andrade <contact@yagoandrade.com>
2024-04-11 16:23:21 -0300

    feat: add search functionality

--- src/components/shared/sidemenu/components/buttons/search-button.tsx
+++ src/components/shared/sidemenu/components/buttons/search-button.tsx
@@ -1,12 +1,63 @@
 import { Button } from "@/components/ui/button";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 import { Search } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
 
 const SidemenuSearchButton = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Button variant="sidemenu" size="sm" className="justify-start gap-x-3 px-6">
-      <Search className="h-4 w-4 text-muted-foreground dark:text-[#cbccd9]" />
-      <p>Search</p>
-    </Button>
+    <>
+      <Button
+        variant="sidemenu"
+        size="sm"
+        className="justify-start gap-x-3 px-6"
+        onClick={() => setOpen(true)}
+      >
+        <Search className="h-4 w-4 text-muted-foreground dark:text-[#cbccd9]" />
+        <p>Search</p>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <Link href="/institution/create">
+              <CommandItem>Create a Institution</CommandItem>
+            </Link>
+            <Link href="/report/create">
+              <CommandItem>Create a Report</CommandItem>
+            </Link>
+            <Link href="/dashboard">
+              <CommandItem>Dashboard</CommandItem>
+            </Link>
+            <Link href="/institutions">
+              <CommandItem>Manage Institutions</CommandItem>
+            </Link>
+            <Link href="/members">
+              <CommandItem>Members</CommandItem>
+            </Link>
+            <Link href="/reports">
+              <CommandItem>Reports</CommandItem>
+            </Link>
+            <Link href="/settings">
+              <CommandItem>Settings</CommandItem>
+            </Link>
+            <Link href="/auth/api/signout">
+              <CommandItem>Sign Out</CommandItem>
+            </Link>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
   );
 };
 
